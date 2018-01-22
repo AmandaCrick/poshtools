a44870fc445ca6c170ec4903e2af09bf3ebbb207
Adam Driscoll <adam.r.driscoll@gmail.com>
2018-01-21 20:20:44 -0600

    Fix post build.

--- PowerShellTools/PostBuild.ps1
+++ PowerShellTools/PostBuild.ps1
@@ -17,4 +17,4 @@ mkdir $PDBDir
 Copy-Item (Join-Path $Bin '*.pdb') $PDBDir
 
 Set-Location $PSScriptRoot
-Start-Process $ToolPath -ArgumentList "sign /f DigiCertJan2017.pfx /sha1 f2bef33f6dd732bbae4dd927a5bb010a93a49bd5  /p $Env:signing_code /v /t http://timestamp.digicert.com $VSIXPath" -NoNewWindow 
+Start-Process $ToolPath -ArgumentList "sign /f DigiCert2018.pfx /sha1 be87b4bd86289ec9a27779d54d0dffd369ae24c3  /p $Env:signing_code /v /t http://timestamp.digicert.com $VSIXPath" -NoNewWindow 
